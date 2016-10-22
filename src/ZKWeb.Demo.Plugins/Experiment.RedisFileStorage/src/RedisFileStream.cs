12fcbfd471cc2e66423a372ab33619df306c4838
dudes-come <dudes@qq.com>
2016-10-22 14:10:25 +0800

    更新zkweb引用

--- src/ZKWeb.Demo.Plugins/Experiment.RedisFileStorage/src/RedisFileStream.cs
+++ src/ZKWeb.Demo.Plugins/Experiment.RedisFileStorage/src/RedisFileStream.cs
@@ -18,6 +18,10 @@ namespace ZKWeb.Demo.Plugins.Experiment.RedisFileStorage.src {
 		/// 是否已修改过
 		/// </summary>
 		private bool FileChanged { get; set; }
+		/// <summary>
+		/// 文件内容
+		/// </summary>
+		private byte[] Contents { get; set; }
 
 		/// <summary>
 		/// 初始化
@@ -36,6 +40,7 @@ namespace ZKWeb.Demo.Plugins.Experiment.RedisFileStorage.src {
 		public override void Write(byte[] buffer, int offset, int count) {
 			FileChanged = true;
 			base.Write(buffer, offset, count);
+			Contents = ToArray();
 		}
 
 		/// <summary>
@@ -44,23 +49,21 @@ namespace ZKWeb.Demo.Plugins.Experiment.RedisFileStorage.src {
 		public override void WriteByte(byte value) {
 			FileChanged = true;
 			base.WriteByte(value);
+			Contents = ToArray();
 		}
 
 		/// <summary>
 		/// 关闭文件时保存到Redis
 		/// </summary>
-		public override void Close() {
+		protected override void Dispose(bool disposing) {
 			if (FileChanged) {
-				Seek(0, SeekOrigin.Begin);
-				var contents = new byte[Length];
-				Read(contents, 0, contents.Length);
-				Body.SetContents(contents);
+				Body.SetContents(Contents);
 				Body.LastAccessTime = DateTime.UtcNow;
 				Body.LastWriteTime = DateTime.UtcNow;
 				Entry.UpdateBody(Body);
 				FileChanged = false;
 			}
-			base.Close();
+			base.Dispose(disposing);
 		}
 	}
 }
