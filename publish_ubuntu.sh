6b62be69a02193de38f7cf5af743d499ffe7cf27
dudes-come <dudes@qq.com>
2018-03-31 09:26:45 +0800

    fix publish_ubuntu.sh for #24832 on corefx

--- publish_ubuntu.sh
+++ publish_ubuntu.sh
@@ -34,3 +34,9 @@ cd ../..
 echo publishing website...
 dotnet ../ZKWeb/Tools/WebsitePublisher.Cmd.NetCore/ZKWeb.Toolkits.WebsitePublisher.Cmd.dll -f netcoreapp2.0 -r src/ZKWeb.Demo.AspNetCore -n "ZKWeb.Demo.Ubuntu" -o "../ZKWeb.Demo.Publish"
 echo "output directory: ../ZKWeb.Demo.Publish"
+
+echo "BUG FIX:"
+echo "this is a workaround to fix #24832 on corefx, please ensure you installed .net core 2.0.6"
+echo "you can check the version from 'dotnet --info'"
+echo "if you using a higher version like 2.0.7, please replace the version in this script"
+cp -f /usr/share/dotnet/shared/Microsoft.NETCore.App/2.0.6/* ../ZKWeb.Demo.Publish/ZKWeb.Demo.Ubuntu
